39dea344860531c8c10baaa6e3976ed595631bdf
joshuastewart417 <joshua.stewart780@gmail.com>
2022-03-15 15:37:43 -0500

    updated html, js, and css

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+window.onscroll = function() {myFunction()};
+function myFunction() {
+    var navbar = document.getElementById("myNavbar");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
+    } else {
+        navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
+    }
+}
+
+window.onload = resetFunction();
+
+function resetFunction() {
+  document.getElementById("contact_form").reset();
+};
